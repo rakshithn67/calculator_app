2f74f610fa2015bd99a6fbc8050499cb6d41a2fc
rakshithn67 <119277332+rakshithn67@users.noreply.github.com>
2022-12-05 19:25:39 +0530

    first update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'screens/home_screen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +14,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Calculator App',
-      home: HomeScreen(),
+      home: AnimatedSplashScreen(
+        backgroundColor: const Color(0xFFE0E0E0),
+        pageTransitionType: PageTransitionType.leftToRightWithFade,
+        splashTransition: SplashTransition.slideTransition,
+        animationDuration: const Duration(seconds: 3),
+        splash: Container(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/calculator.png',
+                width: 60,
+                height: 60,
+              ),
+            ],
+          ),
+        ),
+        nextScreen: const HomeScreen(),
+      ),
     );
   }
 }
